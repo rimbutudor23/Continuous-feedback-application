393983954ba9cd7db7ec778ba406d21d3ec96c60
Tudor Gabriel Rîmbu <tudorgabrielrimbu@Tudors-Mac-mini.local>
2026-01-14 14:54:48 +0200

    routing fix

--- frontend/src/apiClient.jsx
+++ frontend/src/apiClient.jsx
@@ -1,5 +1,4 @@
-export const API_BASE =
-  import.meta.env.VITE_API_BASE || "http://192.168.0.54:4000";
+export const API_BASE = import.meta.env.VITE_API_BASE;
 
 function getToken() {
   return sessionStorage.getItem("authToken") || null;
